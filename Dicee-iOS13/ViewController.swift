02dd5350ff8c9136c947496bd81319991bd6ca86
sohel123146 <63089979+sohel123146@users.noreply.github.com>
2021-08-28 12:45:30 +0530

    Add files via upload

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    //IB outled allow me to reference and ui elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //who         .what  = value
+        *diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+       diceImageView2.alpha = 1.5*
+
+   }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
+
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][rightDiceNumber]
+
+        rightDiceNumber = rightDiceNumber - 1*/
+
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image = diceArray [ Int.random(in: 0...5) ]
+        diceImageView2.image = diceArray [ Int.random(in: 0...5) ]
+
+    }
+    
+}
+
